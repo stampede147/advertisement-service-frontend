bb65da738308d6e63cddcef1d168ae0d5589a8e1
Eugene <stampede147@gmail.com>
2023-08-11 21:36:04 +0300

    added advertisementCard

---
+++ src/components/AdvertisementList/AdvertisementCard.jsx
@@ -0,0 +1,38 @@
+import React from "react";
+
+import './AdvertisementCard.css'
+
+const AdvertisementCard = (props => {
+
+    let advertisement = props.advertisement
+    return (
+        <div className="advertisement-card">
+            <div className="card-image">
+                <img src={advertisement.imgSourceUrl}/>
+            </div>
+            <div className="card-info">
+                <ul className="card-info-list">
+                    <li>{advertisement.title}</li>
+                    <li>{advertisement.price}P</li>
+                    <li>{advertisement.address}</li>
+
+                </ul>
+            </div>
+            <div className="card-actions    ">
+                <button type="button" className="card-update-btn btn">
+                    <span className="card-update-btn-text">
+                        update
+                    </span>
+                </button>
+                <button type="button" className="card-delete-btn btn">
+                    <span className="card-delete-btn-text">
+                        delete
+                    </span>
+                </button>
+            </div>
+        </div>
+
+    )
+})
+
+export default AdvertisementCard;
