705c5bcaf0e61b7186e11beb90c4433e83119240
Eugene <stampede147@gmail.com>
2023-08-16 21:36:21 +0300

    added AdvertisementInfo

--- src/components/AdvertisementInfo/AdvertisementInfo.jsx
+++ src/components/AdvertisementInfo/AdvertisementInfo.jsx
@@ -1,21 +1,22 @@
 import './AdvertisementInfo.css'
 
-const AdvertisementInfo = ({state : advertisement}) => {
+const AdvertisementInfo = ({state: chat}) => {
+    let advertisement = chat.advertisement;
     return (
-        <div className='advertisement-info-root'>
+        <div className='chat-preview-info-root'>
             <div className='advertisement-info-author-name'>
                 {advertisement.owner.name}
             </div>
-            <div className='advertisement-info-title'>
+            <div className='advertisement-info-title__ai'>
                 <div className='advertisement-info-title-title'>
                     {advertisement.title}
                 </div>
                 <div className='advertisement-info-title-price'>
-                    {advertisement.price}
+                    {advertisement.price} P
                 </div>
             </div>
             <div className='advertisement-message'>
-                <span>Привет, как дела11!</span>
+                <span className='advertisement-message-text'>Привет, как дела11!</span>
             </div>
         </div>
     );
