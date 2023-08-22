36e10ad3eb4ca136b862352fdee3833e7e2e73e3
Eugene <stampede147@gmail.com>
2023-08-22 23:37:32 +0300

    changed header to stub component

--- src/pages/MyChatsPage/MyChatsPage.jsx
+++ src/pages/MyChatsPage/MyChatsPage.jsx
@@ -3,24 +3,11 @@ import React, {useEffect} from "react";
 import SearchHeader from "../../components/SearchHeader/SearchHeader";
 import ChatPreviewList from "../../components/ChatPreview/ChatPreviewList";
 import {json} from "react-router-dom";
+import StubComponent from "../../components/StubComponent/StubComponent";
 
 const MyChats = ({state}) => {
-    const options = {
-        method: "GET",
-        headers: new Headers({"Content-Type": 'application/json'}),
-        mode: 'cors'
-    };
-
-
-    useEffect(() => {
-        fetch('http://localhost:8080/api/v1/chats/3', options)
-            .then(rawResponse => rawResponse.json())
-            .then(jsonResponse => console.log(jsonResponse));
-    }, []);
-
-
     return (
-        <GenericPage header={SearchHeader}
+        <GenericPage header={StubComponent}
                      content={ChatPreviewList}
                      state={state}
         />
