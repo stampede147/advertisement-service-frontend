b264072b859687288ed3129f94e1d2d25cc19d3c
Eugene <stampede147@gmail.com>
2023-08-22 23:34:31 +0300

    reworked chat page

--- src/pages/ChatPage/ChatPage.jsx
+++ src/pages/ChatPage/ChatPage.jsx
@@ -2,12 +2,13 @@ import GenericPage from "../GenericPage/GenericPage";
 import SearchHeader from "../../components/SearchHeader/SearchHeader";
 import Chat from "../../components/Chat/Chat";
 import React  from "react";
+import StubComponent from "../../components/StubComponent/StubComponent";
 
 const ChatPage = (props) => {
 
     return(
         <GenericPage state={props.state}
-                     header={SearchHeader}
+                     header={StubComponent}
                      content={Chat}
         />
     );
