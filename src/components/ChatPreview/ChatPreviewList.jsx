a022bc1f0da345ad14e2618fa4c0e2e7525fe85d
Eugene <stampede147@gmail.com>
2023-08-22 23:38:41 +0300

    reworked, added new functions

--- src/components/ChatPreview/ChatPreviewList.jsx
+++ src/components/ChatPreview/ChatPreviewList.jsx
@@ -1,34 +1,65 @@
-import React  from "react";
+import React, {useEffect, useState} from "react";
 import "./ChatPreviewList.css"
 import {Link} from "react-router-dom";
 import ChatPreview from "./ChatPreview";
+import {getChatsByUserId} from "../../api/chatApi";
+import {wait} from "@testing-library/user-event/dist/utils";
+import {get} from "axios";
+import State from "../../redux/state";
 
 const ChatPreviewList = ({state, ...props}) => {
 
-    const CURRENT_URL = window.location.pathname + '/';
+    const [chatPreviews, setChatPreviews] = useState([])
 
-    let chatsContent = state.chatsPage.content;
-    const mappedChats = chatsContent.map(chat => {
+    const [number, setNumber] = useState(0)
+
+    useEffect(() => {
+        const fetchData = async () => {
+            const response = await getChatsByUserId(1);
+
+            let content = response.content;
+
+            content.map(chat => {
+                chat.advertisement.imgSourceUrl = State.imgSourceUrl
+                // for (let participant of chat.advertisement.participants) {
+                //     participant.imgSourceUrl = State.imgSourceUrl
+                // }
+            })
+            setChatPreviews([...content])
+        }
+
+        fetchData().catch(console.error)
+
+    }, []);
+
+
+    const mappedChats = chatPreviews.map(chat => {
+        console.log(chat)
         return (
-            <Link key={chat.chatId}
+            <Link key={chat.id}
                   className='chat-preview-link'
-                  to={CURRENT_URL + chat.chatId}>
+                  to='#'>
                 <ChatPreview chat={chat}/>
             </Link>
         )
     })
-    return (
-            <div className="chat-preview-list-root">
-                <div className="chat-preview-list-header">
-                    <h1>Messages</h1>
-                </div>
-                <div className="chat-preview-list-content">
-                    {mappedChats}
-                </div>
 
+    function onClick() {
+        console.log(number)
+        setNumber(number+1);
+    }
+    return (
+        <div className="chat-preview-list-root">
+            <div className="chat-preview-list-header">
+                <h1>Messages</h1>
             </div>
+            <div className="chat-preview-list-content">
+                {mappedChats}
+                <button onClick={onClick}>CLICK ME</button>
+            </div>
+
+        </div>
     )
-        ;
 }
 
 export default ChatPreviewList;
