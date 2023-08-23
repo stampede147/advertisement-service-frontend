46a540203f0e17d75c4a0484b4e782063364f052
Eugene <stampede147@gmail.com>
2023-08-23 22:21:01 +0300

    reworked, added new switch case

--- src/reducers/chatMessageReducer.js
+++ src/reducers/chatMessageReducer.js
@@ -1,33 +1,14 @@
 export default function chatMessageReducer(state, action) {
-    console.log('called reducer!');
-
     switch (action.type) {
         case 'add' :
             return [...state, action.data]
+
+        case 'replace_state' :
+            return [
+                ...action.data
+            ]
     }
 
 
 }
 
-function createChatMessage(data) {
-    return {
-        messageId: 22,
-        senderId: 15,
-
-    }
-}
-
-
-//chat messageId, newText
-function updateChatMessage(state) {
-
-}
-
-
-//chat id
-function deleteChatMessage(state) {
-
-}
-
-function findChatMessages(state) {
-}
