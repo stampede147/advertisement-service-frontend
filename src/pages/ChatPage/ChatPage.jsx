dc538d1959b9ec565c5d9c321e006d2fdae2b20d
Eugene <stampede147@gmail.com>
2023-08-25 22:43:55 +0300

    reworked, added new component

--- src/pages/ChatPage/ChatPage.jsx
+++ src/pages/ChatPage/ChatPage.jsx
@@ -4,15 +4,15 @@ import Chat from "../../components/Chat/Chat";
 import React from "react";
 import StubComponent from "../../components/StubComponent/StubComponent";
 import {useLocation, useNavigate} from "react-router-dom";
+import IndexContainer from "../../container/IndexContainer/IndexContainer";
+import ProfileBar from "../../components/ProfileBar/ProfileBar";
 
 const ChatPage = () => {
 
-    let chatDetails = useLocation().state;
-
     return (
-        <GenericPage state={chatDetails}
+        <GenericPage state={null}
                      header={StubComponent}
-                     content={Chat}
+                     content={() => <IndexContainer navbar={ProfileBar} content={Chat}/>}
         />
     );
 }
