69e4d4c4148c21b4a95893611c19268bc721cbc2
Eugene <stampede147@gmail.com>
2023-08-25 22:42:40 +0300

    changed package location

--- src/components/ProfileBar/ProfileBar.jsx
+++ src/components/ProfileBar/ProfileBar.jsx
@@ -1,18 +1,18 @@
-import React  from "react";
+import React, {useEffect} from "react";
 import './ProfileBar.css'
+import State from "../../redux/state";
 
 const ProfileBar = () => {
-    let props = {user: {name: "aboba"}}
-    const user = props.user;
+
     return (
         <div className="profile-side-bar">
-            <div className="profile-author-details">
-                <div className="profile-icon icon">
-                    <img src={props.user.sourceUrl}/>
+            <div className="profile-side-bar-header">
+                <div className="profile-icon">
+                    <img src={State.imgSourceUrl}/>
                 </div>
                 <div className="profile-author-name">
                         <span className="author-name-text">
-                            {user.name}
+                            EUGENIY
                         </span>
                 </div>
                 <div className="profile-reviews">
@@ -21,23 +21,23 @@ const ProfileBar = () => {
                     </a>
                 </div>
             </div>
-            <div className="profile-redirect-elements">
-                <div className="main-profile-page element-bar">
+            <ul className="profile-side-bar-navigation-list">
+                <li className="main-profile-page element-bar">
                     <a href="#">
                         <p>my page</p>
                     </a>
-                </div>
-                <div className="main-profile-advertisements element-bar">
+                </li>
+                <li className="main-profile-advertisements element-bar">
                     <a href="#">
                         <p>my advertisements</p>
                     </a>
-                </div>
-                <div className="main-profile-messages element-bar">
+                </li>
+                <li className="main-profile-messages element-bar">
                     <a href="#">
                         <p>my messages</p>
                     </a>
-                </div>
-            </div>
+                </li>
+            </ul>
 
         </div>
     )
