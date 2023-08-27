95b3c0d06eb230375d767884e26d4315dc6f4ee5
Eugene <stampede147@gmail.com>
2023-08-27 22:47:20 +0300

    reworked using hook

--- src/pages/MyAdvertisementsPage/MyAdvertisementsPage.jsx
+++ src/pages/MyAdvertisementsPage/MyAdvertisementsPage.jsx
@@ -1,26 +1,33 @@
-import AdvertisementPreviewList from "../../components/AdvertisementPreview/AdvertisementPreviewList";
+import MyAdvertisementPreviewList from "../../components/AdvertisementPreview/MyAdvertisementPreviewList";
 import SinglePageWrapper from "../SinglePageWrapper/SinglePageWrapper";
 import React, {useEffect, useState} from "react";
 import StubComponent from "../../components/StubComponent/StubComponent";
 import {useNavigate} from "react-router-dom";
 import * as advertisementApi from "../../api/advertisementApi";
-import advertisement from "../../components/Advertisement/Advertisement";
 import PATHNAMES from "../../constants/PATHNAMES";
 import IndexContainer from "../../container/IndexContainer/IndexContainer";
 import ProfileBar from "../../components/ProfileBar/ProfileBar";
+import * as useApi from "../../api/userApi";
+import LOCALSTORAGE_KEYS from "../../constants/LOCALSTORAGE_KEYS";
+import NavigationPanel from "../../components/IndexNavigationPanel/NavigationPanel";
 
-const MyAdvertisementsPage = () => {
+ export default () => {
 
     const [loading, setLoading] = useState(true);
     const [advertisements, setAdvertisements] = useState([]);
     const navigate = useNavigate();
 
     useEffect(() => {
-        advertisementApi.getPageByUserId(1)
-            .then(resp => {
-                setLoading(false);
-                setAdvertisements(resp.content);
-            });
+
+        async function fetchData() {
+
+            const user = await useApi.getUserByUsername(localStorage.getItem(LOCALSTORAGE_KEYS.USERNAME_KEY));
+            const advertisements = await advertisementApi.getPageByUserId(user.id)
+
+            setAdvertisements(advertisements.content == null ? [] : advertisements.content)
+            setLoading(false);
+        }
+        fetchData();
     }, []);
 
 
@@ -29,19 +36,16 @@ const MyAdvertisementsPage = () => {
     state.onClickRedirect = (advertisementId) => {
         navigate(PATHNAMES.ADVERTISEMENTS + '/' + advertisementId);
     };
-    state.onClickRedirect = state.onClickRedirect.bind(MyAdvertisementsPage);
+    state.onClickRedirect = state.onClickRedirect.bind(this);
 
     return (
         <SinglePageWrapper
-            header={StubComponent}
-            content={() => {
-                return <IndexContainer {...state}
-                                       navbar={ProfileBar}
-                                       content={AdvertisementPreviewList}
-                />
-            }}
+            header={NavigationPanel}
+            content={() => <IndexContainer {...state}
+                                           navbar={ProfileBar}
+                                           content={MyAdvertisementPreviewList}
+            />
+            }
         />
     )
-}
-
-export default MyAdvertisementsPage;
+};
